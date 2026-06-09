10f02644f721cf5d7430dfef529d97d45d1e3743
Faheer Hakim <r0820158@student.thomasmore.be>
2026-06-09 15:22:05 +0200

    import components is toegevoegd in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
-<script setup></script>
+<script setup>
+import IcecreamDashboard from'./components/icecreamDashboard.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="app">
+  <div class="kolom">
+  <IcecreamDashboard />
+
+  </div>
+  </div>
 </template>
 
 <style scoped></style>
